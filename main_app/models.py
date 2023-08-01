02c87dc77c574c8974510b6b88ea713cbb248434
vivienkp <vivienkp@gmail.com>
2023-07-31 21:00:49 -0400

    added feeding models, urls, templates

--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,47 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
 
 # Create your models here.
 class Finch(models.Model):
-    name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    age = models.IntegerField()
+  name = models.CharField(max_length=100)
+  breed = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField()
 
-# Changing this instance method does not impact the dataabase
-# therefore no migrations needed
-    def __str__(self):
-        return f'{self.name} ({self.id})'
-    
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'finch_id': self.id})
+  # Changing this instance method
+  # does not impact the database, therefore
+  # no makemigrations is necessary
+  def __str__(self):
+    return f'{self.name} ({self.id})'
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'finch_id': self.id})
+
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  # Create a finch_id FK
+  finch = models.ForeignKey(
+    Finch,
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
-
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -18,7 +18,9 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch })
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', {
+     'finch': finch,'feeding_form': feeding_form })
 
 class FinchCreate(CreateView):
   model = Finch
@@ -32,3 +34,18 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using 
+  # the data that was submitted in the form
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # We want a model instance, but
+    # we can't save to the db yet
+    # because we have not assigned the
+    # cat_id FK.
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
